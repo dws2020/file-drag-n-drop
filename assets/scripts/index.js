5c07551ceeb1bb0b547403281b33e3a9ac14ff68
2015-I-030Z <kikaku@shinonome.shinkin.jp>
2025-03-24 14:26:51 +0900

    ファイル一覧表示部分作成

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,32 @@
-console.log("script");
+
+function getAttachmentFiles(inputId) {
+    const inputElement = document.querySelector(`#${inputId}`);
+    if (inputElement.type !== "file") return false;
+    return inputElement.files;
+}
+
+/**
+ * アップロード予定のファイルをファイル一覧に描画する
+ * @param {*} files 
+ */
+function listAttachedFiles(files) {
+    const fileList = document.querySelector("#file-list-ul");
+    fileList.innerHTML = "";
+    for (const file of files) {
+        const li = document.createElement("li");
+        li.classList.add("file-list-li");
+        li.textContent = file.name;
+        fileList.append(li);
+    }
+}
+
+/**
+ * 
+ */
+window.addEventListener("DOMContentLoaded", () => {
+    const droppedFiles = document.querySelector("#dropped-files");
+    droppedFiles.addEventListener("change", () => {
+        const files = getAttachmentFiles("dropped-files");
+        listAttachedFiles(files);
+    });
+});
