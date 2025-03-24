ddfee9181390ca92fd810d00443f77aac67d45c6
2015-I-030Z <kikaku@shinonome.shinkin.jp>
2025-03-24 15:19:06 +0900

    feat: 削除ボタンのUI作成

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,15 +9,37 @@ function getAttachmentFiles(inputId) {
  * アップロード予定のファイルをファイル一覧に描画する
  * @param {*} files 
  */
-function listAttachedFiles(files) {
+function renderAttachedFiles(files) {
     const fileList = document.querySelector("#file-list-ul");
     fileList.innerHTML = "";
+    let i = 0;
     for (const file of files) {
         const li = document.createElement("li");
         li.classList.add("file-list-li");
         li.textContent = file.name;
+
+        // 削除ボタン
+        const removeButton = createRemoveButton(i)
+
+        li.append(removeButton);
         fileList.append(li);
+        i++;
     }
+}
+
+function createRemoveButton(i) {
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove-button");
+    const iconImg = document.createElement("img");
+    iconImg.src= "./assets/images/trashIcon.svg";
+    removeButton.append(iconImg);
+    removeButton.dataset.fileIndex = i;
+
+    // 削除イベント
+    removeButton.addEventListener("click", (event) => {
+        console.log("remove", event.currentTarget.dataset.fileIndex);
+    });
+    return removeButton;
 }
 
 /**
@@ -27,6 +49,6 @@ window.addEventListener("DOMContentLoaded", () => {
     const droppedFiles = document.querySelector("#dropped-files");
     droppedFiles.addEventListener("change", () => {
         const files = getAttachmentFiles("dropped-files");
-        listAttachedFiles(files);
+        renderAttachedFiles(files);
     });
 });
