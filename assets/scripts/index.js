54fdc2ead11b0f6b46481efa268c9e9c2f75669e
ds <d326@yahoo.co.jp>
2025-03-26 23:50:58 +0900

    js1本化

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -155,3 +155,28 @@ function toggleReasonButtonAbility() {
 		}
 	}
 }
+
+const dropArea = document.querySelector("#drop-area");
+dropArea.addEventListener("dragover", (event) => onDragOver(event));
+dropArea.addEventListener("dragleave", (event) => onDragLeave(event));
+dropArea.addEventListener("drop", (event) => onDrop(event));
+
+function onDragOver(event) {
+	event.preventDefault();
+	dropArea.classList.add("_drag-over");
+}
+
+function onDragLeave(event) {
+	event.preventDefault();
+	dropArea.classList.remove("_drag-over");
+}
+
+function onDrop(event) {
+	event.preventDefault();
+	dropArea.classList.remove("_drag-over");
+	const files = event.dataTransfer.files;
+	for (const file of files) {
+		selectedFiles.push(file);
+	}
+	updateState();
+}
