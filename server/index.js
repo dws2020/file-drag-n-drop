ffcc219b48150229848d18e75e7314ddc026bd5c
2015-I-030Z <kikaku@shinonome.shinkin.jp>
2025-03-26 13:43:21 +0900

    バックエンドでファイルを受け取る機能実装

--- server/index.js
+++ server/index.js
@@ -1,16 +1,41 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
+const multer = require('multer');
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, '/tmp/uploads');
+	},
+	filename: (req, file, cb) => {
+		const uniqueSuffix = Date.now() + '-' * Math.round(Math.random() * 1E9);
+		cb(null, file.fieldname + '-' + uniqueSuffix);
+		console.log('filed-name: ', fieldname);
+	}
+});
+const upload = multer({storage: storage});
 
 app.get("/", (req, res) => {
-	console.log("home");
 	res.send("Hello World");
 });
 
-app.post("/upload", (req, res) => {
+app.post("/upload", upload.array('files', 10), (req, res) => {
 	res.header("Access-Control-Allow-Origin", "*");
+	
+	// ファイルがアップロードされていない場合
+	if (!req.files) {
+		return res.status(400).send("No file attached");
+	}
+
+	// 持込事由パラメータ
 	const reason = req.query.reason;
 	res.send(reason);
+
+	// uploaded files
+	console.log(req.files);
+	for (const file of req.files) {
+		console.log('filePath', file.path);
+	}
 });
 
 app.listen(PORT);
