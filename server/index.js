062756c77c8632bb5b626ef29485500046e9c1d0
ds <d326@yahoo.co.jp>
2025-03-26 21:47:57 +0900

    一時ファイル保存先をプロジェクト内に作成

--- server/index.js
+++ server/index.js
@@ -1,35 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const path = require('path');
-const cors = require('cors');
-const multer = require('multer');
+const path = require("node:path");
+const cors = require("cors");
+const multer = require("multer");
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, 'C:/Users/01000/tmp/uploads');
+		cb(null, path.join(__dirname, "tmp/uploads"));
 	},
 	filename: (req, file, cb) => {
-		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-		const extension = file.originalname.split('.').pop();
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		const extension = file.originalname.split(".").pop();
 		const originalName = decodeURIComponent(file.originalname);
 		// 同じファイル名を認めないようにすれば、suffixいらない？
 		cb(null, `${originalName}-${uniqueSuffix}.${extension}`);
-	}
+	},
 });
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 // CORSミドルウェア設定
-app.use(cors({
-	origin: '*',
-}));
+app.use(
+	cors({
+		origin: "*",
+	}),
+);
 
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
-app.post("/upload", upload.array('files', 10), (req, res) => {
+app.post("/upload", upload.array("files", 10), (req, res) => {
 	res.header("Access-Control-Allow-Origin", "*");
-	
+
 	// ファイルがアップロードされていない場合
 	if (!req.files) {
 		return res.status(400).send("No file attached");
@@ -37,13 +39,13 @@ app.post("/upload", upload.array('files', 10), (req, res) => {
 
 	// 持込事由パラメータ
 	const reason = decodeURIComponent(req.query.reason);
-	
+
 	// uploaded files
 	// console.log(req.files);
 	for (const file of req.files) {
-		console.log('reason: ', reason)
-		console.log('filePath: ', file.path);
-		console.log('originalName: ', decodeURIComponent(file.originalname));
+		console.log("reason: ", reason);
+		console.log("filePath: ", file.path);
+		console.log("originalName: ", decodeURIComponent(file.originalname));
 	}
 	res.send("OK");
 });
